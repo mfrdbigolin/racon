0f871c74d0ad05fb998201733418607dc6984ecb
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2020-12-31 23:12:27 -0300

    Dual-license Racon under the GPLv3+ and the MIT

--- src/parser.c
+++ src/parser.c
@@ -1,19 +1,6 @@
 /* parser.c -- parser frontend
- * Copyright (C) 2020 Matheus Fernandes Bigolin
- * Contact e-mail: <mfrdrbigolin@disroot.org>
- */
-
-/* This file is part of Racon.
- *   Racon is free software (libre); you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
- * option) any later version.
- *   Racon is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.
- *   See the GNU General Public License for more details, you should have
- * received a copy of it along with Racon; otherwise, see
- * <https://www.gnu.org/licenses/gpl.html>.
+ * Copyright (C) 2020 Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
+ * SPDX-License-Identifier: GPL-3.0-or-later OR MIT
  */
 
 #include "parser.h"
